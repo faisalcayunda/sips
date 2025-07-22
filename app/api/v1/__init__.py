e3f69ce4e4563e47d8bac8846ae036dd18f968ec
faisalcayunda <faisal.nugraha.c@gmail.com>
2025-07-22 20:38:43 +0700

    refactor: rename tags

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -14,7 +14,7 @@ from app.api.v1.routes import (
 router = APIRouter()
 router.include_router(auth_router, tags=["Auth"])
 router.include_router(file_router, tags=["Files"])
-router.include_router(forestry_schema_router, tags=["forestry Schema"])
+router.include_router(forestry_schema_router, tags=["Forestry Schema"])
 router.include_router(regional_router, tags=["Regional"])
 router.include_router(piaps_router, tags=["Piaps"])
 router.include_router(proposal_forestry_router, tags=["Proposal forestry"])
